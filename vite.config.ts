dab1823028a9f63b5d6ef8ebe7bd68f348aaa67b
john <aarontu803@gmail.com>
2023-09-05 13:36:51 +0800

    fix: vite-config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'weblayout-practice-week6',
+  base:'/weblayout-practice-week6/',
   plugins: [
     vue(),
   ],
